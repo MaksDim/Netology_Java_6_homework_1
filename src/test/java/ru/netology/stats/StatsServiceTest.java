cc48636cc78792f8f3f5b52829d74322764b345e
Максимов Дмитрий <demasig@yandex.ru>
2023-05-23 13:41:31 +0400

    first commit

---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,92 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+    @Test
+    void shouldCalculateSumOfAllSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+
+        int actual = service.calculateSumOfAllSales(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateAverageMonthlySales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+
+        int actual = service.calculateAverageMonthlySales(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldNumMonthPeakSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+
+        int actual = service.calculateAverageMonthlySales(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldNumMonthMaxSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+
+        int actual = service.numMonthMaxSales(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldNumMonthMinSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+
+        int actual = service.numMonthMinSales(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateQuantityMonthMaxSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.calculateQuantityMonthMaxSales(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateQuantityMonthMinSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.calculateQuantityMonthMinSales(sales);
+
+        assertEquals(expected, actual);
+
+    }
+}
